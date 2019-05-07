33a26d013b8cd3c236b58351fc06ce171017f251
Devin Rowland <devinrowland12@gmail.com>
2019-05-07 12:57:05 -0700

    Updates on PA1

--- pa1/PA1/PA1/PA1.cpp
+++ pa1/PA1/PA1/PA1.cpp
@@ -1,85 +1,153 @@
-#include "PA1.h"
 
+#include "PA1.h"
 
 //PA #1 TOOD: Generates a Huffman character tree from the supplied text
 HuffmanTree<char>* PA1::huffmanTreeFromText(vector<string> data)
 {
+	//Builds a Huffman Tree from the supplied vector of strings.
+	//This function implement's Huffman's Algorithm as specified in the
+	//book.
 
-    //Builds a Huffman Tree from the supplied vector of strings.
-    //This function implement's Huffman's Algorithm as specified in the
-    //book.
+	//In order for your tree to be the same as mine, you must take care 
+	//to do the following:
+	//1.	When merging the two smallest subtrees, make sure to place the 
+	//      smallest tree on the left side!
 
-    //In order for your tree to be the same as mine, you must take care 
-    //to do the following:
-    //1.	When merging the two smallest subtrees, make sure to place the 
-    //      smallest tree on the left side!
-	//store frequencies in hashtable
+	// counting and storing frequencies in hashtable
 
 	unordered_map<char, int> frequencies{};
 	for (auto word : data)
 	{
 		for (auto ch : word)
 		{
+			// counts the frequencies
 			frequencies[ch]++;
 		}
 	}
 
+	// put into trees
 	//maintains huffman tree forest 
-    priority_queue < HuffmanTree<char>*, vector<HuffmanTree<char>*>, TreeComparer> forest{};
+	priority_queue < HuffmanTree<char>*, vector<HuffmanTree<char>*>, TreeComparer> forest{};
 
+	//loop through frequency distributions
 	for (auto kvp : frequencies)
 	{
+		//kvp first char, kvp second frequency(weight)	
+		// pushes "trees" into the forest with key, value pair
 		forest.push(new HuffmanTree<char>(kvp.first, kvp.second));
 	}
 
-	// This is how you merge two trees, connects them both using a linker node... see example for details
-	HuffmanTree<char>* smaller = forest.top();
-	forest.pop();
-	HuffmanTree<char>* larger = forest.top();
-	forest.pop();
-	forest.push(new HuffmanTree<char>{ smaller, larger });
+	//merge two trees
+	// while there is more than one tree in the forest
+	while (forest.size() > 1)
+	{
+		HuffmanTree<char>*smaller = forest.top();
+		forest.pop();
+		HuffmanTree<char>*larger = forest.top();
+		forest.pop();
 
-    //TODO: shouldn't return nullptr
-    return forest.top();
+		//merge two trees
+		forest.push(new HuffmanTree<char>{ smaller,larger });
+	}
+
+
+	return forest.top(); // return tree
 }
 
 //PA #1 TODO: Generates a Huffman character tree from the supplied encoding map
 //NOTE: I used a recursive helper function to solve this!
 HuffmanTree<char>* PA1::huffmanTreeFromMap(unordered_map<char, string> huffmanMap)
 {
+	//Generates a Huffman Tree based on the supplied Huffman Map. Recall that a 
+	//Huffman Map contains a series of codes(e.g. 'a' = > 001).Each digit(0, 1) 
+	//in a given code corresponds to a left branch for 0 and right branch for 1.
 
-    //Generates a Huffman Tree based on the supplied Huffman Map.Recall that a 
-    //Huffman Map contains a series of codes(e.g. 'a' = > 001).Each digit(0, 1) 
-    //in a given code corresponds to a left branch for 0 and right branch for 1.
-    return nullptr;
+	HuffmanInternalNode<char>* root = new HuffmanInternalNode<char>{ nullptr, nullptr };
+	HuffmanInternalNode<char>* current = root;
+
+
+	string path;
+	char value;
+
+	for (auto kvp : huffmanMap)
+	{
+		path = kvp.second;
+		value = kvp.second[kvp.second.length() - 1];
+
+		for (int i = 0; i < path.length() - 1; i++)
+		{
+			char ch = path[i];
+			if (ch == '0')
+			{
+				if (current->getLeftChild() == NULL)
+				{
+					current->setLeftChild(new HuffmanInternalNode<char>{ nullptr,nullptr });
+				}
+
+				current = dynamic_cast<HuffmanInternalNode<char>*>(current->getLeftChild());
+			}
+
+			else
+			{
+				if (current->getRightChild() == nullptr)
+				{
+					current->setRightChild(new HuffmanInternalNode<char>{ nullptr,nullptr });
+				}
+
+				current = dynamic_cast<HuffmanInternalNode<char>*>(current->getRightChild());
+			}
+
+		}
+
+		char last_ch = (path[path.length() - 1]);
+
+		if (last_ch == '0')
+		{
+			current->setLeftChild(new HuffmanLeafNode<char>{ value,1 });
+			current = root;
+		}
+		else
+		{
+			current->setRightChild(new HuffmanLeafNode<char>{ value,1 });
+			current = root;
+		}
+	}
+
+	HuffmanTree<char>* forest = new HuffmanTree<char>{ root };
+
+	return forest;
 }
 
-void huffmanEncodingMapFromTreeHelper(unordered_map<char, string>& map,
-	HuffmanNode<char>* node,
-	string encoding)
+void huffmanEncodingMapFromTreeHelper(unordered_map<char, string>&map,
+	HuffmanNode<char>*node, string encoding)
 {
 	if (node->isLeaf() == false)
 	{
-		//not a leaf, make recursive calls
-		HuffmanInternalNode<char>* root =
-			dynamic_cast<HuffmanInternalNode<char>*>(node);
+		//make recursive calls
+		// more dynamic casts: from generic node into an internal node
+		HuffmanInternalNode<char>*root = dynamic_cast<HuffmanInternalNode<char>*>(node);
 		huffmanEncodingMapFromTreeHelper(map, root->getLeftChild(), encoding + "0");
 		huffmanEncodingMapFromTreeHelper(map, root->getRightChild(), encoding + "1");
 	}
 	else
 	{
-		//this is a leaf. This means that we have a complete mapping for this character
-		//this converts node, which is a huffman node into a leafnode
-		HuffmanLeafNode<char>* root = dynamic_cast<HuffmanLeafNode<char>*>(node);
+		// is leaf
+		// this converts node, which is a Huffman node into a leaf node
+		// syntax:                   convert      want			          what we want to convert
+		HuffmanLeafNode<char>*root = dynamic_cast<HuffmanLeafNode<char>*>(node);
 		map[root->getValue()] = encoding;
 		return;
 	}
 }
 
+
 //PA #1 TODO: Generates a Huffman encoding map from the supplied Huffman tree
 //NOTE: I used a recursive helper function to solve this!
 unordered_map<char, string> PA1::huffmanEncodingMapFromTree(HuffmanTree<char> *tree)
 {
+	// pre order walk of the tree!
+	// will need to do dynamic_casting
+
 	HuffmanNode<char> *node = tree->getRoot();
 	if (node->isLeaf() == true)
 	{
@@ -87,51 +155,132 @@ unordered_map<char, string> PA1::huffmanEncodingMapFromTree(HuffmanTree<char> *t
 		leaf = (HuffmanLeafNode<char> *)node;
 	}
 
-    //Generates a Huffman Map based on the supplied Huffman Tree.  Again, recall 
-    //that a Huffman Map contains a series of codes(e.g. 'a' = > 001).Each digit(0, 1) 
-    //in a given code corresponds to a left branch for 0 and right branch for 1.  
-    //As such, a given code represents a pre-order traversal of that bit of the 
-    //tree.  I used recursion to solve this problem.
-    unordered_map<char, string> result{};
+	unordered_map<char, string> result{};
 	huffmanEncodingMapFromTreeHelper(result, tree->getRoot(), "");
-    return result;
+	return result;
 }
 
 //PA #1 TODO: Writes an encoding map to file.  Needed for decompression.
 void PA1::writeEncodingMapToFile(unordered_map<char, string> huffmanMap, string file_name)
 {
-    //Writes the supplied encoding map to a file.  My map file has one 
-    //association per line (e.g. 'a' and 001 would yield the line "a001")
+	//Writes the supplied encoding map to a file.  My map file has one 
+	//association per line (e.g. 'a' and 001 would yield the line "a001")
+	ofstream outputFile{ file_name };
+	for (auto kvp : huffmanMap)
+	{
+		outputFile << kvp.first;
+		for (auto path : kvp.second)
+		{
+			outputFile << path;
+		}
+		outputFile << "\n";
+	}
+
+	outputFile.close();
+
 }
 
 //PA #1 TODO: Reads an encoding map from a file.  Needed for decompression.
 unordered_map<char, string> PA1::readEncodingMapFromFile(string file_name)
 {
-    //Creates a Huffman Map from the supplied file.Essentially, this is the 
-    //inverse of writeEncodingMapToFile.  
-    unordered_map<char, string> result{};
-    return result;
+	//Creates a Huffman Map from the supplied file.Essentially, this is the 
+	//inverse of writeEncodingMapToFile.  
+	// turn back in to key,value pair
+	unordered_map<char, string> result{};
+	string line;
+	ifstream inputFile{ file_name };
+	string path;
+	char key;
+	while (inputFile.is_open())
+	{
+		getline(inputFile, line);
+		for (auto ch : line)
+		{
+			if (ch != '0' && ch != '1')
+			{
+				key = ch;
+			}
+			else
+			{
+				path.push_back(ch);
+			}
+		}
+		result.emplace(key, path);
+		path.clear();
+
+		if (line.empty())
+		{
+			inputFile.close();
+		}
+	}
+
+
+
+
+	return result;
 }
 
 //PA #1 TODO: Converts a vector of bits (bool) back into readable text using the supplied Huffman map
 string PA1::decodeBits(vector<bool> bits, unordered_map<char, string> huffmanMap)
 {
-    //Uses the supplied Huffman Map to convert the vector of bools (bits) back into text.
-    //To solve this problem, I converted the Huffman Map into a Huffman Tree and used 
-    //tree traversals to convert the bits back into text.
-    ostringstream result{};
-    return result.str();
+	//Uses the supplied Huffman Map to convert the vector of bools (bits) back into text.
+	//To solve this problem, I converted the Huffman Map into a Huffman Tree and used 
+	//tree traversals to convert the bits back into text
+	// call build tree from map
+	ostringstream result{};
+	HuffmanTree<char>* tree;
+
+	tree = huffmanTreeFromMap(huffmanMap);
+	/*
+	for (auto path : bits)
+	{
+		if (path == false)
+		{
+			// go left
+		}
+		else
+		{
+			// go right
+		}
+	}
+	*/
+
+
+
+	return result.str();
 }
 
 //PA #1 TODO: Using the supplied Huffman map compression, converts the supplied text into a series of bits (boolean values)
 vector<bool> PA1::toBinary(vector<string> text, unordered_map<char, string> huffmanMap)
 {
-    vector<bool> result{};
+	// take each char and make it equal to binary representation
+	vector<bool> result{};
 
-	//after you have the tree, walk down the tree until you see a leaf node
-	
-	//if leaf node:
+	for (auto str : text)
+	{
+		for (auto ch : str)
+		{
+			for (auto kvp : huffmanMap)
+			{
+				if (ch == kvp.first)
+				{
+					for (auto path : kvp.second)
+					{
+						if (path == '0')
+						{
+							result.push_back(0);
+						}
+						else
+						{
+							result.push_back(1);
+						}
+					}
 
+				}
+			}
+		}
 
-    return result;
+	}
+
+	return result;
 }
--- pa3/starter code/cpp/lecture/lecture/main.cpp
+++ pa3/starter code/cpp/lecture/lecture/main.cpp
@@ -13,41 +13,81 @@ int main(void)
 	string map_num;
 	string deliv_num;
 
-	cout << "*** Route Planner ***" << endl;
-	cout << "Enter maps file: ";
-	cin >> map_num;
-	cout << "Enter destinations file: ";
-	cin >> deliv_num;
+	//cout << "*** Route Planner ***" << endl;
+	//cout << "Enter maps file: ";
+	//cin >> map_num;
+	//cout << "Enter destinations file: ";
+	//cin >> deliv_num;
 
 	//Putting the map file into a 2D vector
-	CsvStateMachine mapping{ map_num };
+	CsvStateMachine mapping{ "map1.txt" };
 	vector<vector<string>> map_vector = mapping.processFile();
 
 	//Putting the delivery file in a 2D vector even though we dont need it, its makes it easier on me
-	CsvStateMachine destinations{ deliv_num };
+	CsvStateMachine destinations{ "deliveries1.txt" };
 	vector<vector<string>> dest_vector = destinations.processFile();
 
 	//CityGraph that all vertexs and connections will be registered
 	CityGraph graph{};
 
-	// Will also add to this vector for an easy search to see if the character is already in the CityGraph
-	vector<string> vertexs{};
+	//Create a unordred map to get all vertexs to put in the city graph
+	unordered_map<string, int> vertexs{};
 	
 	// Adds vertexs before they all get connected
 	// Goes through first row while checking to see if the number is already in the graph
 	int i = 0;
 	int j = 0;
 
-	graph.addVertex(map_vector[i][0]);
-
-	while (i <= map_vector.size())
+	/*
+	for (int i = 0; i < map_vector.size(); i++)
 	{
-		if()
+		for (int j = 0; j <= 1; j++)
+		{
+			if (vertexs.find(map_vector[i][j]) == vertexs.end())
+			{
+				graph.addVertex(map_vector[i][j]);
+			}
+		}
+	}
+	*/
+
+	//Add vertexs only in the deliveries
+	for (i = 0; i < dest_vector.size(); i++)
+	{
+		if (vertexs.find(dest_vector[i][0]) == vertexs.end())
+		{
+			graph.addVertex(dest_vector[i][0]);
+		}
+	}
+
+	i = 0;
+
+	//Connect all the vertexs
+	while (i < map_vector.size())
+	{
+		int num = 0;
+		string character;
+
+		if (vertexs.find(map_vector[i][j]) == vertexs.end() && vertexs.find(map_vector[i][j + 1]) == vertexs.end())
+		{
+			character = map_vector[i][2];
+			num = stoi(character);
+			graph.connectVertex(map_vector[i][0], map_vector[i][1], num);
+		}
+		i++;
 	}
 
 	// After the first row is done, go through the secondst 
 	
-	auto distances = graph.computeShortestPath("a");
-	auto mst = graph.computeMinimumSpanningTree("a");
+	auto distances = graph.computeShortestPath(dest_vector[0][0]);
+	auto mst = graph.computeMinimumSpanningTree(dest_vector[0][0]);
+	
+	int total_distance = 0;
+	//Now we are going to travel through the deliveries text file
+	for (i = 0; i < dest_vector.size(); i++)
+	{
+		total_distance += mst
+	}
+
 	return 0;
 }
